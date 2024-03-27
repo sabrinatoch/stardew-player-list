88c5c54ae9763b4f3a56caffb0227d2bf671af9c
Sabrina Tochkov <sabrina.tochkov@gmail.com>
2024-03-27 08:37:37 -0400

    Added current version of Web IV A02

---
+++ stochkov_H40A02/classes/class-leader.php
@@ -0,0 +1,105 @@
+<?php
+declare(strict_types=1);
+class Leader {
+    private string $name;
+    private string $email_address;
+    private string $password;
+
+    function __construct(string $name, string $email_address, string $password) {
+        $this->name = $name;
+        $this->email_address = $email_address;
+        $this->password = $password;
+    } // __construct()
+
+    static function email_error(string $email, array $leaders) : string {
+        if ($email === "")
+            return "Email address is required.";
+        else if (!preg_match('/^[a-z][\w.%+-]*@[\w.-]+\.[a-z]{2,}$/i', $email))
+            return "Invalid email address.";
+        $leaders = array_values($leaders);
+        if (sizeof($leaders) > 0) {
+            foreach ($leaders as $leader) {
+                $leader_arr = explode("~", $leader);
+                if ($email === $leader_arr[1])
+                    return "An account with that email already exists.";
+            } // foreach
+        } // if
+        return "";
+    } // email_error()
+
+    static function password_error(string $pass) : string {
+        if ($pass === "")
+            return "Password is required.";
+        else if (!preg_match("/[0-9]/", $pass))
+            return "Password must contain a number.";
+        else if (!preg_match("/[a-z]/", $pass))
+            return "Password must contain a lowercase character.";
+        else if (!preg_match("/[A-Z]/", $pass))
+            return "Password must contain an uppercase character.";
+        else if (preg_match("/\s/", $pass))
+            return "Password cannot contain spaces.";
+        else if (!preg_match("/[^A-Za-z0-9]/", $pass))
+            return "Password must contain a special character.";
+        else if (strlen($pass) < 8 || strlen($pass) > 16)
+            return "Password must be 8-16 characters long.";
+        else
+            return "";
+    } // password_error()
+
+    static function confirm_password(string $pass, string $confirm) : string {
+        return ($pass === $confirm) ? "" : "Passwords must match.";
+    } // confirm_password()
+
+    static function login_error(string $email, string $pass, array $leaders) : string {
+        if ($email === "" || $pass === "")
+            return "Please enter your email and password.";
+        $leaders = array_values($leaders);
+        if (sizeof($leaders) > 0) {
+            foreach ($leaders as $leader) {
+                $leader_arr = explode("~", $leader);
+                if ($email === $leader_arr[1]) {
+                    if (password_verify($pass, $leader_arr[2]))
+                        return "";
+                } // if email exists
+            } // foreach
+        } // if
+        return "Invalid email or password.";
+    } // login_error()
+
+    static function get_leader_name(string $email, array $leaders) : string {
+        $leaders = array_values($leaders);
+        if (sizeof($leaders) > 0) {
+            foreach ($leaders as $leader) {
+                $leader_arr = explode("~", $leader);
+                if ($email === $leader_arr[1]) {
+                   return $leader_arr[0];
+                } // if
+            } // foreach
+        } // if
+        return "";
+    } // get_leader_name()
+
+    function create_new_leader() : string {
+        return $this->name . "~" . $this->email_address . "~" . $this->password;
+    } // create_new_leader()
+
+    // setters & getters
+    function get_name() : string {
+        return $this->name;
+    }
+    function set_name(string $name) : void {
+        $this->name = $name;
+    }
+    function get_email_address() : string {
+        return $this->email_address;
+    }
+    function set_email_address(string $email_address) : void {
+        $this->email_address = $email_address;
+    }
+    function get_password() : string {
+        return $this->password;
+    }
+    function set_password(string $password) : void {
+        $this->password = $password;
+    }
+} // Leader class
